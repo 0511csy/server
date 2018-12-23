780e5f620fb480313237927ce09c9e06421cb689
csy <277376753@qq.com>
2018-12-23 11:32:26 +0800

    更新

--- server.js
+++ server.js
@@ -23,16 +23,22 @@ console.log('HTTP 路径为\n' + path)
   if(path == '/'){
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/css; charset=utf-8')
-    response.write('css link')
-    response.write('script')	  
+    response.write('<!DOCTYPE>\n<html>'  + 
+      '<head><link rel="stylesheet" href="/style.js">' +
+      '</head><body>'  +
+      '<h1>你好</h1>' +
+      '<script src="/script.html"></script>' +
+      '</body></html>')
     response.end()
   }else if(path == '/main.js'){
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/javascript; charset=utf-8')
+    response.write('alert("这是JS执行的")')
     response.end()
   }else if(path == '/style.css'){
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/html; charset=utf-8')
+    response.write('body{background-color: #ddd;}h1{color: red;}')
     response.end()
   }else{
     response.statusCode = 404
